ee8ae3c174b9d91abfecc45f926bb3d8de1f494e
Bastian Schattenberg <git@bestyan.de>
2019-04-23 19:16:17 +0200

    increase canvas size and improve drawing logic
    drawing separate lines is now possible
    refactor Canvas.js a little

--- fake-artist/src/Canvas.js
+++ fake-artist/src/Canvas.js
@@ -5,7 +5,11 @@ import React, { Component } from "react";
  * TODO: write a test to ensure that it doesnt have a border
  */
 class Canvas extends Component {
+  // whether the user is currently drawing (holding the mouse down)
   isDrawing = false;
+  // holds the coordinates of the line that is currently being drawn
+  // [{x: 0, y: 0}, {...}]
+  currentLine = [];
 
   constructor() {
     super();
@@ -18,67 +22,61 @@ class Canvas extends Component {
         x: 0,
         y: 0
       },
-      drawHistory: []
+      drawnLines: []
     };
 
-    this.setDraw = this.setDraw.bind(this);
+    this.startDrawing = this.startDrawing.bind(this);
+    this.stopDrawing = this.stopDrawing.bind(this);
+    this.getLineCoordinates = this.getLineCoordinates.bind(this);
     this.draw = this.draw.bind(this);
   }
 
-  setDraw = isDrawing => {
-    this.isDrawing = isDrawing;
-  };
+  startDrawing = () => {
+    this.isDrawing = true;
+    this.currentLine = [];
+  }
+
+  stopDrawing = () => {
+    // further mouse movement doesnt draw on canvas
+    this.isDrawing = false;
+    const { drawnLines } = this.state;
+
+    // add completed line to drawnLines state
+    drawnLines.push(this.currentLine);
+
+    this.setState({
+      drawnLines: drawnLines
+    });
+  }
 
   draw = event => {
     if (!this.isDrawing) {
       return;
     }
 
-    //small timesaver for typing ^-^
+    // small timesavers for typing ^-^
     const canvas = this.canvas;
 
-    // position of the canvas
-    const { x, y } = canvas.getBoundingClientRect();
-    // position of the mouse in browser window
-    const { clientX, clientY } = event;
+    // get coordinates
+    const { startPoint, endPoint } = this.getLineCoordinates(event);
 
-    const relativeMousePos = {
-      x: clientX - x,
-      y: clientY - y
-    };
-
-    const drawHistory = this.state.drawHistory;
-    drawHistory.push(relativeMousePos);
-
-    const historyEntries = drawHistory.length;
-    const startEntry = historyEntries - 2 >= 0 ? historyEntries - 2 : 0;
-    const endEntry = historyEntries - 1;
-    const lineStart = {
-      x: drawHistory[startEntry].x,
-      y: drawHistory[startEntry].y
-    };
-    const lineEnd = {
-      x: drawHistory[endEntry].x,
-      y: drawHistory[endEntry].y
-    };
-
-    // drawing
+    // prepare drawing
     const context = canvas.getContext("2d");
     // set pen properties
     context.strokeStyle = "#FF0000";
     context.lineJoin = "round";
     context.lineWidth = 5;
-    
+
     // draw line
     context.beginPath();
-    context.moveTo(lineStart.x, lineStart.y);
-    context.lineTo(lineEnd.x, lineEnd.y);
+    context.moveTo(startPoint.x, startPoint.y);
+    context.lineTo(endPoint.x, endPoint.y);
     context.closePath();
     context.stroke();
 
+    // TODO: remove when coordinate display is removed
     this.setState({
-      mouse: relativeMousePos,
-      drawHistory: drawHistory
+      mouse: endPoint
     });
   };
 
@@ -87,9 +85,11 @@ class Canvas extends Component {
     return (<div>
       <canvas
         id="draw-area"
-        onMouseDown={event => { this.setDraw(true); this.draw(event); }}
-        onMouseUp={event => this.setDraw(false)}
-        onMouseLeave={event => this.setDraw(false)}
+        width="600px" // must be set like this, css width/height scales instead of making the canvas larger/smaller
+        height="450px"
+        onMouseDown={event => { this.startDrawing(); this.draw(event); }}
+        onMouseUp={event => this.stopDrawing()}
+        onMouseLeave={event => this.stopDrawing()}
         onMouseMove={this.draw}
         ref={canvas => this.canvas = canvas} />
       <div>
@@ -98,6 +98,37 @@ class Canvas extends Component {
       </div>
     </div>);
   }
+
+  getLineCoordinates = (event) => {
+    // position of the canvas
+    const { x, y } = this.canvas.getBoundingClientRect();
+    // position of the mouse in browser window
+    const { clientX, clientY } = event;
+
+    const relativeMousePos = {
+      x: clientX - x,
+      y: clientY - y
+    };
+
+    const currentLine = this.currentLine;
+    currentLine.push(relativeMousePos);
+
+    const numberOfPoints = currentLine.length;
+    const startPointIndex = numberOfPoints - 2 >= 0 ? numberOfPoints - 2 : 0;
+    const endPointIndex = numberOfPoints - 1;
+
+    const startPoint = {
+      x: currentLine[startPointIndex].x,
+      y: currentLine[startPointIndex].y
+    };
+    const endPoint = {
+      x: currentLine[endPointIndex].x,
+      y: currentLine[endPointIndex].y
+    };
+
+    return { startPoint, endPoint };
+  };
+
 }
 
 export default Canvas;
