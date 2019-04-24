95e848b161d14bede1b23636dd187e634c9a2687
Bastian Schattenberg <git@bestyan.de>
2019-04-24 21:20:28 +0200

    add lines sync between clients
    when drawing in one browser window, the completed line appears in other browser windows too
    GET /fetch-state fetches the _completed_ lines on the canvas
    PUT /put-line to add a completed line

--- fake-artist/src/backend/server.js
+++ fake-artist/src/backend/server.js
@@ -1,37 +1,45 @@
+
+const Constants = require("../Constants");
+
 const express = require('express');
 const server = express();
 // automatically parse request body to json. accessible via request.body
 // https://stackoverflow.com/questions/10005939/how-do-i-consume-the-json-post-data-in-an-express-application
 server.use(express.json());
 
+// allow requests from port 3000
 const cors = require("cors");
 const corsOptions = {
-    origin: 'http://localhost:3000'
+    origin: `${Constants.CLIENT_ADDRESS}`
 };
+
+console.log(corsOptions);
 
 server.use(cors(corsOptions));
 
 const drawnLines = [];
-const port = 3001;
 
-server.get(`/canvas`, (request, response) => {
+server.get(`${Constants.FETCH_STATE}`, (request, response) => {
     response.json({
         lines: drawnLines
     });
 });
 
-server.put(`/putline`, (request, response) => {
+server.put(`${Constants.PUT_LINE}`, (request, response) => {
     const data = request.body;
     drawnLines.push(data.line);
 
     console.log(data);
+    response.json({
+        status: "success"
+    });
 });
 
-server.listen(port, (err) => {
+server.listen(Constants.SERVER_PORT, (err) => {
     if (err) {
         // port is already in use or something else went wrong
         return console.log('something bad happened', err);
     }
 
-    console.log(`server is listening on ${port}`);
+    console.log(`server is listening on ${Constants.SERVER_PORT}`);
 });
