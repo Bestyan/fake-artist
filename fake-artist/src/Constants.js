f3185267fed637afe4471b8f55def335d5fcd59b
Bastian Schattenberg <git@bestyan.de>
2019-05-19 18:28:25 +0200

    refactor Constants.js

--- fake-artist/src/Constants.js
+++ fake-artist/src/Constants.js
@@ -2,46 +2,10 @@
 // node server
 const SERVER_URL = "http://localhost";
 const SERVER_PORT = 3001;
-const SERVER_ADDRESS = `${SERVER_URL}:${SERVER_PORT}`;
 
 // react frontend server
 const CLIENT_URL = "http://localhost";
 const CLIENT_PORT = 3000;
-const CLIENT_ADDRESS =  `${CLIENT_URL}:${CLIENT_PORT}`;
-
-const RESPONSE_STATUS = "status";
-const RESPONSE_MESSAGE = "message";
-
-// PUT request to submit player name
-const PUT_NAME = "/choose-name";
-const PUT_NAME_PLAYER_ID = "player-id";
-const PUT_NAME_PLAYER_NAME = "player-name";
-const PUT_NAME_PLAYER_COLOR = "player-color";
-
-// GET request to get game status (started or not started)
-const GET_GAME_START = "/game-start";
-const GET_GAME_START_PLAYERS = "players";
-const GET_GAME_START_STATUS = "game-status";
-
-// GET request to get active player
-const GET_ACTIVE_PLAYER = "/whose-turn-is-it"
-const GET_ACTIVE_PLAYER_ACTIVE_PLAYER = "active-player";
-
-// POST request to update the line that is currently being drawn
-const POST_LINE = "/update-incomplete-line";
-const POST_LINE_PLAYER_ID = "player-id";
-const POST_LINE_INCOMPLETE_LINE = "incomplete-line";
-
-// GET request to get current canvas/game state
-const GET_STATE = "/fetch-state";
-// json object key to get canvas lines
-const GET_STATE_LINES = "lines";
-
-// PUT request to add a finished line
-const PUT_LINE = "/put-line";
-const PUT_LINE_PLAYER_ID = "player-id";
-const PUT_LINE_FINISHED_LINE = "finished-line";
-const PUT_LINE_ACTIVE_PLAYER = "active-player";
 
 // ============================================================
 // Game Logic Constants
@@ -53,6 +17,7 @@ const PHASE_PRE_GAME = "choose-name-and-wait-for-players";
 const PHASE_PRE_DRAW = "question-master-declares-topic-and-term";
 // players take turns drawing exactly one line each
 const PHASE_DRAWING_TURNS = "drawing-turns";
+
 const PHASE_ORDER = [
     PHASE_PRE_GAME,
     PHASE_PRE_DRAW,
@@ -84,47 +49,53 @@ const PHASE_ORDER_PRE_DRAW = [
 ];
 
 // es5 exports because node does not support es6 exports yet and I dont want to introduce babel just for this
-// TODO refactor this to put all the info directly in here
 module.exports = {
 
     SERVER_URL: SERVER_URL,
     SERVER_PORT: SERVER_PORT,
-    SERVER_ADDRESS: SERVER_ADDRESS,
+    SERVER_ADDRESS: `${SERVER_URL}:${SERVER_PORT}`,
 
     CLIENT_URL: CLIENT_URL,
     CLIENT_PORT: CLIENT_PORT,
-    CLIENT_ADDRESS: CLIENT_ADDRESS,
+    CLIENT_ADDRESS: `${CLIENT_URL}:${CLIENT_PORT}`,
 
-    RESPONSE_STATUS: RESPONSE_STATUS,
-    RESPONSE_MESSAGE: RESPONSE_MESSAGE,
+    RESPONSE_STATUS: "status",
+    RESPONSE_MESSAGE: "message",
 
-    PUT_NAME: PUT_NAME,
-    PUT_NAME_PLAYER_NAME: PUT_NAME_PLAYER_NAME,
-    PUT_NAME_PLAYER_ID: PUT_NAME_PLAYER_ID,
-    PUT_NAME_PLAYER_COLOR: PUT_NAME_PLAYER_COLOR,
+    // PUT request to submit player name
+    PUT_NAME: "/choose-name",
+    PUT_NAME_PLAYER_NAME: "player-name",
+    PUT_NAME_PLAYER_ID: "player-id",
+    PUT_NAME_PLAYER_COLOR: "player-color",
 
-    GET_GAME_START: GET_GAME_START,
-    GET_GAME_START_PLAYERS: GET_GAME_START_PLAYERS,
-    GET_GAME_START_STATUS: GET_GAME_START_STATUS,
+    // GET request to get game status (started or not started)
+    GET_GAME_START: "/game-start",
+    GET_GAME_START_PLAYERS: "players",
+    GET_GAME_START_STATUS: "game-status",
 
+    // POST requeust to get the assigned role
     POST_ROLE: "/fetch-player-role",
     POST_ROLE_PLAYER_ID: "player-id",
     POST_ROLE_PLAYER_ROLE: "player-role",
 
-    GET_ACTIVE_PLAYER: GET_ACTIVE_PLAYER,
-    GET_ACTIVE_PLAYER_ACTIVE_PLAYER: GET_ACTIVE_PLAYER_ACTIVE_PLAYER,
+    // GET request to get active player
+    GET_ACTIVE_PLAYER: "/whose-turn-is-it",
+    GET_ACTIVE_PLAYER_ACTIVE_PLAYER: "active-player",
 
-    POST_LINE: POST_LINE,
-    POST_LINE_PLAYER_ID: POST_LINE_PLAYER_ID,
-    POST_LINE_INCOMPLETE_LINE: POST_LINE_INCOMPLETE_LINE,
+    // POST request to update the line that is currently being drawn
+    POST_LINE: "/update-incomplete-line",
+    POST_LINE_PLAYER_ID: "player-id",
+    POST_LINE_INCOMPLETE_LINE: "incomplete-line",
 
-    PUT_LINE: PUT_LINE,
-    PUT_LINE_PLAYER_ID: PUT_LINE_PLAYER_ID,
-    PUT_LINE_FINISHED_LINE: PUT_LINE_FINISHED_LINE,
-    PUT_LINE_ACTIVE_PLAYER: PUT_LINE_ACTIVE_PLAYER,
+    // PUT request to submit a finished line
+    PUT_LINE: "/finish-line",
+    PUT_LINE_PLAYER_ID: "player-id",
+    PUT_LINE_FINISHED_LINE: "finished-line",
+    PUT_LINE_ACTIVE_PLAYER: "active-player",
 
-    GET_STATE: GET_STATE,
-    GET_STATE_LINES: GET_STATE_LINES,
+    // GET request to get current canvas/game state
+    GET_STATE: "/fetch-canvas-state",
+    GET_STATE_LINES: "lines",
 
     // === Main Game Phases ===
     PHASE_PRE_GAME: PHASE_PRE_GAME,
